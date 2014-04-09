7840cd4c2737d33cd3fb0bebee1fbe6f899900b0
y-hashimoto <y-hashimoto@192.168.0.215>
2014-04-09 12:14:07 +0900

    オプションメニュー追加

--- RepositoryTest/src/jp/co/hassy/repositorytest/MainActivity.java
+++ RepositoryTest/src/jp/co/hassy/repositorytest/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.co.hassy.repositorytest;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowHomeEnabled(false);
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setDisplayHomeAsUpEnabled(false);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		
+		return true;
 	}
 }

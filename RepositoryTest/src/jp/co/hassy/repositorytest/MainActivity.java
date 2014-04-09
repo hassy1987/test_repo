d9727347088244e8352016adad2ff241957c449b
y-hashimoto <y-hashimoto@192.168.0.215>
2014-04-09 11:20:00 +0900

    "Hello World"表示

--- RepositoryTest/src/jp/co/hassy/repositorytest/MainActivity.java
+++ RepositoryTest/src/jp/co/hassy/repositorytest/MainActivity.java
@@ -2,6 +2,7 @@ package jp.co.hassy.repositorytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		// HelloWorld
+		TextView textView = (TextView) findViewById(R.id.textview_helloworld);
+		textView.setText(R.string.hello_world);
 	}
 }
